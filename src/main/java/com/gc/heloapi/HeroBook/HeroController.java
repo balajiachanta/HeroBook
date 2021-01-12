30c4119f51a50a2bb44ef011f04991895755872b
BalajiAchanta <balajiachanta@gmail.com>
2021-01-11 18:42:54 -0600

    Get All hero names implemented

--- src/main/java/com/gc/heloapi/HeroBook/HeroController.java
+++ src/main/java/com/gc/heloapi/HeroBook/HeroController.java
@@ -3,14 +3,18 @@ package com.gc.heloapi.HeroBook;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class HeroController {
 
-
     @GetMapping("/heros")
-    public void listHerosNames(){
-
-
+    public List<String> listHerosNames(){
+        List<String> heroList = new ArrayList<>();
+        heroList.add("superman");
+        heroList.add("spiderman");
+        return heroList;
     }
 
 }
--- src/test/java/com/gc/heloapi/HeroBook/HeroControllerTest.java
+++ src/test/java/com/gc/heloapi/HeroBook/HeroControllerTest.java
@@ -6,11 +6,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
 public class HeroControllerTest {
-
 
     @Autowired
     private MockMvc mockMvc;
@@ -19,16 +19,14 @@ public class HeroControllerTest {
     List all hero names  - Get Api which return all heros
         - Table - heros with id and names
      */
-
     @Test
     public void testGetAllHeroNames() throws Exception {
-
-        mockMvc.perform(get("/heros")).andExpect(status().isOk());
-
-
-
-
+        mockMvc.perform(get("/heros")).andExpect(status().isOk())
+                .andExpect(jsonPath("$[0]").value("superman"))
+                .andExpect(jsonPath("$[1]").value("spiderman"));
     }
+
+
 
 
 
