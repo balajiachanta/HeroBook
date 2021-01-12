e26d7704b5369e8e1f1e7d56c0b6ea2fd9d3fd2c
BalajiAchanta <balajiachanta@gmail.com>
2021-01-11 18:13:53 -0600

    Initial commit

---
+++ src/test/java/com/gc/heloapi/HeroBook/HeroControllerTest.java
@@ -0,0 +1,22 @@
+package com.gc.heloapi.HeroBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest
+public class HeroControllerTest {
+
+    /*
+    List all hero names  - Get Api which return all heros
+        - Table - heros with id and names
+     */
+
+    @Test
+    public void testGetAllHeroNames(){
+
+
+    }
+
+
+
+}
